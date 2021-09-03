a1d3f52378e5f308428bc4042a88f82c4869872b
Found Pan Tiger <renzholy@gmail.com>
2021-09-03 14:24:34 +0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
-import { grommet, Grommet } from 'grommet'
+import { grommet, Grommet, Nav } from 'grommet'
+import * as Icons from 'grommet-icons'
 import { AppProps } from 'next/app'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Grommet theme={grommet}>
+      <GlobalStyle />
+      <Nav direction="row" background="brand" pad="small">
+        <Icons.Home />
+        <Icons.Notification />
+        <Icons.ChatOption />
+      </Nav>
       <Component {...pageProps} />
     </Grommet>
   )
