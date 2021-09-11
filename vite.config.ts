08bc394a2bbb7649f22ebb3f797d48e9e71e7f3d
Found Pan Tiger <renzholy@gmail.com>
2021-09-11 15:55:25 +0800

    use react-scripts for build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import reactJsx from 'vite-react-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), reactJsx()],
-  build: {
-    assetsDir: '.',
-  },
 })
