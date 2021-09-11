a2393db618090c028acd7613840e27aa2bc6bbf6
Found Pan Tiger <renzholy@gmail.com>
2021-09-11 20:25:01 +0800

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,26 @@
-import { Anchor, Box, Nav } from 'grommet'
-import { ReactNode } from 'react'
+import { Anchor, Box, Nav, TextInput } from 'grommet'
+import { ReactNode, useState } from 'react'
 import * as Icon from 'grommet-icons'
+import useSWR from 'swr'
+import { arweave } from '../utils/arweave'
 import AnchorLink from './anchor-link'
 
 export default function Layout(props: { children: ReactNode }) {
+  const [keyword, setKeyword] = useState('')
+  const { data } = useSWR(keyword ? ['search', keyword] : null, async () => {
+    try {
+      const [block, transaction, wallet] = await Promise.all([
+        arweave.transactions.get(keyword).catch(() => null),
+        arweave.blocks.get(keyword).catch(() => null),
+        arweave.wallets.getBalance(keyword).catch(() => null),
+      ])
+      return { transaction, block, wallet }
+    } catch {
+      return undefined
+    }
+  })
+  console.log(data)
+
   return (
     <>
       <Nav direction="row" background="dark-1" pad="small" justify="center">
@@ -12,12 +29,23 @@ export default function Layout(props: { children: ReactNode }) {
           pad={{ left: 'medium', right: 'medium' }}
           fill="horizontal"
           direction="row"
-          justify="between"
+          align="center"
+          gap="20px"
         >
           <Box width="min-content">
             <AnchorLink to="/" color="light-1">
               SCAR
             </AnchorLink>
+          </Box>
+          <Box fill="horizontal">
+            <TextInput
+              size="xsmall"
+              icon={<Icon.Search />}
+              plain={true}
+              placeholder="block, transaction"
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
+            />
           </Box>
           <Anchor href="https://github.com/renzholy/scar" target="_blank" style={{ lineHeight: 0 }}>
             <Icon.Github />
